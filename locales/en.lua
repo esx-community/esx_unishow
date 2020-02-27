15265c168b7957916fc81ea9796e11dd4b16ad95
rex2630 <rex2630@gmail.com>
2020-02-27 02:06:21 +0100

    Added english language

---
+++ locales/en.lua
@@ -0,0 +1,20 @@
+Locales ['cs'] = {
+  -- Client
+  ['press_key'] = 'press ~INPUT_CONTEXT~ for choosing show',
+  ['dance_menu'] = 'dance menu',
+  ['dancer_menu'] = 'dancer menu',
+  ['person'] = 'person',
+  ['position_menu'] = 'position menu',
+  -- Config
+  ['stripter'] = 'stripter',
+  ['stripter_fat'] = 'fat stripter',
+  ['pole_dance'] = 'pole dance',
+  ['lap_dance'] = 'lap dance',
+  ['scene_center'] = 'center scene',
+  ['left_background'] = 'left back scene',
+  ['right_background'] = 'right back scene',
+  ['background_centered'] = 'center of back scene',
+  ['left_front'] = 'left front scene',
+  ['right_front'] = 'right front scene',
+  ['stripters'] = 'stripters'
+}
