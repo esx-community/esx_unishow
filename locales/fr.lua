09d39bcce0cbe4f3bb68f72cdadbc497b354af26
rex2630 <rex2630@gmail.com>
2020-02-27 01:59:04 +0100

    Added French locale

---
+++ locales/fr.lua
@@ -0,0 +1,20 @@
+Locales ['fr'] = {
+  -- Client
+  ['press_key'] = 'appuyez sur ~INPUT_CONTEXT~ pour choisir les shows',
+  ['dance_menu'] = 'dance Menu',
+  ['dancer_menu'] = 'dancer Menu',
+  ['person'] = 'personne',
+  ['position_menu'] = 'position Menu',
+  -- Config
+  ['stripter'] = 'stripteaseuse',
+  ['stripter_fat'] = 'stripteaseuse Fat',
+  ['pole_dance'] = 'pole_dance',
+  ['lap_dance'] = 'lap dance',
+  ['scene_center'] = 'scene centre',
+  ['left_background'] = 'fond scene gauche',
+  ['right_background'] = 'fond scene droit',
+  ['background_centered'] = 'Fond scene centre',
+  ['left_front'] = 'scene avant gauche',
+  ['right_front'] = 'scene avant droit',
+  ['stripters'] = 'stripteaseurs'
+}
