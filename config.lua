44852350226921702c4291ebb6b4b80383c5d665
rex2630 <rex2630@gmail.com>
2020-02-27 01:58:29 +0100

    Added locales support

--- config.lua
+++ config.lua
@@ -2,58 +2,58 @@ Config = {}
 Config.Locale = 'fr'
 
 Config.Ped = {
-  {label='Stripteaseuse 1',type=5, hash='s_f_y_stripper_01'},
-  {label='Stripteaseuse 2',type=5, hash='s_f_y_stripper_02'},
-  {label='Stripteaseuse Fat',type=5, hash='a_f_m_fatcult_01'},
-  {label='Stripteaseurs 1',type=5, hash='a_m_y_musclbeac_01'},
-  {label='Stripteaseurs SM',type=5, hash='a_m_m_acult_01'},
+  {label='_U('stripter') 1', type=5, hash='s_f_y_stripper_01'},
+  {label='_U('stripter') 2', type=5, hash='s_f_y_stripper_02'},
+  {label='_U('stripter_fat')', type=5, hash='a_f_m_fatcult_01'},
+  {label='_U('stripters') 1', type=5, hash='a_m_y_musclbeac_01'},
+  {label='_U('stripters') SM', type=5, hash='a_m_m_acult_01'},
 }
 
 Config.Salle = {
   scene1 = {
-    label = 'Fond scene gauche',
+    label = '_U('left_background')',
     vector3(101.8, -1289.7, 29.2, 290.5),
   },
   scene2 = {
-    label = 'Fond scene droit',
+    label = '_U('right_background')',
     vector3(104.6, -1295.2, 29.2, 297.2),
   },
   scene3 = {
-    label = 'Fond scene centre',
+    label = '_U('background_centered')',
     vector3(104.0, -1292.1, 29.2, 298.9),
   },
   scene4 = {
-    label = 'scene centre',
+    label = '_U('scene_center')',
     vector3(107.3, -1290.2, 28.8, 297.3),
   },
   scene5 = {
-    label = 'scene avant gauche',
+    label = '_U('left_front')',
     vector3(112.0, -1286.2, 28.4, 30.0),
   },
   scene6 = {
-    label = 'scene avant droit',
+    label = '_U('right_front')',
     vector3(113.2, -1288.2, 28.4, 211.8),
   }
 }
 
 Config.Dict = {
   show1={
-    label = 'Lap dance 1',
+    label = '_U('lap_dance') 1',
     name = 'mini@strip_club@lap_dance@ld_girl_a_song_a_p1',
     anim ='ld_girl_a_song_a_p1_f',
   },
   show2={
-    label = 'Lap dance 2',
+    label = '_U('lap_dance') 2',
     name = 'mini@strip_club@lap_dance@ld_girl_a_song_a_p2',
     anim ='ld_girl_a_song_a_p2_f',
   },
   show3={
-    label = 'Lap dance 3',
+    label = '_U('lap_dance') 3',
     name = 'mini@strip_club@lap_dance@ld_girl_a_song_a_p3',
     anim ='ld_girl_a_song_a_p3_f',
   },
   show4={
-    label = 'pole_dance',
+    label = '_U('pole_dance')',
     name = 'mini@strip_club@pole_dance@pole_dance1',
     anim ='pd_dance_0',
   },
