be3ca02f635e4ad99f0fc8639c99f36bf9191553
rex2630 <rex2630@gmail.com>
2020-02-27 01:13:37 +0100

    Fixed indent

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,9 +7,9 @@ description 'ESX STRIP'
 version '1.0.0'
 
 client_scripts {
- 	'@es_extended/locale.lua',
- 	'locales/en.lua',
- 	'locales/cs.lua',
+  '@es_extended/locale.lua',
+  'locales/en.lua',
+  'locales/cs.lua',
   'config.lua',
   'client.lua',
  }
