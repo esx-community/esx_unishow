5135319386a9b4620969f05f2efc8b55f925f9ba
rex2630 <rex2630@gmail.com>
2020-02-27 02:02:53 +0100

    Added czech locale

---
+++ locales/cs.lua
@@ -0,0 +1,20 @@
+Locales ['cs'] = {
+  -- Client
+  ['press_key'] = 'stiskni ~INPUT_CONTEXT~ pro vyber show',
+  ['dance_menu'] = 'menu tancu',
+  ['dancer_menu'] = 'menu tanecnic',
+  ['person'] = 'osoba',
+  ['position_menu'] = 'menu pozice',
+  -- Config
+  ['stripter'] = 'stripterka',
+  ['stripter_fat'] = 'tlusta stripterka',
+  ['pole_dance'] = 'tanec u tyce',
+  ['lap_dance'] = 'tanec na kline',
+  ['scene_center'] = 'stred',
+  ['left_background'] = 'leve zadni pozadi',
+  ['right_background'] = 'prave zadni pozadi',
+  ['background_centered'] = 'stred pozadi',
+  ['left_front'] = 'leve popredi',
+  ['right_front'] = 'prave popredi',
+  ['stripters'] = 'stripterky'
+}
