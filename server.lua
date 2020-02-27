071ac13c8c5706ef795196d45b2fb073ce3e3706
rex2630 <rex2630@gmail.com>
2020-02-27 01:11:03 +0100

    Minor cleanup

--- server.lua
+++ server.lua
@@ -1,24 +1,22 @@
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-local places ={}
+local places = {}
 
 function joueurs(ped)
-
 	local players = GetPlayers()
+	
 	for k,v in pairs(players) do
-		print (v)
 		TriggerClientEvent('esx_unishow:StopDance',v,ped)
 	end
 end
+
 RegisterServerEvent('esx_unishow:RegisterShow')
 AddEventHandler('esx_unishow:RegisterShow',function(param)
-	print (param.label)
 	table.insert(places,{
 		label = param.label,
 		ped   = param.ped
 		})	
 end)
-
 
 RegisterServerEvent('esx_unishow:DeleteShow')
 AddEventHandler('esx_unishow:DeleteShow',function(label)
